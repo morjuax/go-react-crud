38844c1f62b01ccb8fed337a8979508c4feb6e64
Juan Moreno <jcarlmorenos@falabella.cl>
2025-01-11 15:46:24 -0300

    fix: port dynamic

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/morjuax/go-react-crud/utils"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	} 
+
 	app := fiber.New()
 
 	app.Static("/", "./client/dist")
@@ -19,8 +25,6 @@ func main() {
 		})
 	})
 
-	app.Listen(":3000")
-
-	
+	app.Listen(":" + port)
 	utils.Print("Server on port 3000")
 }
