348d96fbc59b3299d2d1f7a1f43b4fd9526f4c07
Juan Moreno <morjua19@gmail.com>
2025-01-11 18:03:51 -0300

    chore: conexion mongodb success

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/morjuax/go-react-crud/utils"
-	"os"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -14,6 +19,18 @@ func main() {
 	} 
 
 	app := fiber.New()
+	
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	coll := client.Database("gomongodb").Collection("users")
+	coll.InsertOne(context.TODO(), bson.D{{
+		Key: "name", 
+		Value: "morjuax",
+	}})
 
 	app.Static("/", "./client/dist")
 
